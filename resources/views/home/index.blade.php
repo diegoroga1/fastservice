86dfc51c00f844d6edfd75d4e7f28309ab466ad7
diegoroga1 <diegoroga1993@gmail.com>
2018-11-22 22:35:41 +0100

    Inicio desarrollando...

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,216 @@
+<!DOCTYPE HTML>
+<!--
+	Telephasic by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Services</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="/css/main.css" />
+    <link rel="stylesheet" href="/css/index.css" />
+    <link rel="stylesheet" href="/css/font-awesome.min.css" />
+</head>
+<body class="homepage is-preload">
+<div id="page-wrapper">
+
+    <!-- Header -->
+    <div id="header-wrapper">
+        <div id="header" class="container">
+
+            <!-- Logo -->
+            <h1 id="logo"><a href="index.html">Inicio</a></h1>
+
+            <!-- Nav -->
+            <nav id="nav">
+                <ul>
+                    <li><a href="left-sidebar.html">Servicios</a></li>
+                    <li class="break"><a href="right-sidebar.html">Contacto</a></li>
+                </ul>
+            </nav>
+
+        </div>
+
+        <!-- Hero -->
+        <section id="hero" class="container">
+            <header>
+                <h2>Solicita tu servicio</h2>
+            </header>
+            <p>Indicanos que es lo que necesitas y te mandaremos un presupuesto.</p>
+            <ul class="actions">
+                <li><a href="#" class="button">Seleccionar servicio</a></li>
+            </ul>
+        </section>
+
+    </div>
+
+    <!-- Features 1 -->
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <section class="col-6 col-12-narrower feature">
+
+                    <header>
+                        <div class="image-wrapper first">
+                            <a href="#" class="image featured first ">
+                                <div class="bgd-1">
+                                    <h2 class="center-vertical">Limpieza</h2>
+                                </div>
+
+                            </a>
+
+                        </div>
+
+
+                    </header>
+
+
+                </section>
+                <section class="col-6 col-12-narrower feature">
+                    <div class="image-wrapper first">
+                        <a href="#" class="image featured first ">
+                            <div class="bgd-2">
+                                <h2 class="center-vertical">Pintor</h2>
+                            </div>
+
+                        </a>
+
+                    </div>
+                </section>
+                <section class="col-6 col-12-narrower feature">
+                    <div class="image-wrapper first">
+                        <a href="#" class="image featured first ">
+                            <div class="bgd-3">
+                                <div class="bgd-3-over">
+
+                                </div>
+                               <div class="center-vertical">
+                                   <h2 >Masajista</h2>
+                               </div>
+                            </div>
+
+
+                        </a>
+
+                    </div>
+                </section>
+                <section class="col-6 col-12-narrower feature">
+                    <div class="image-wrapper first">
+                        <a href="#" class="image featured first ">
+                            <div class="bgd-4">
+                                <h2 class="center-vertical">Albañil</h2>
+                            </div>
+
+                        </a>
+
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <!-- Promo -->
+    <div id="promo-wrapper">
+        <section id="promo" class="col-12 " >
+            <h2></h2>
+            <a href="#" class="button w-100">¿Quieres ofrecer tu servicio?</a>
+            <p class="text-center">Click para rellenar solicitud</p>
+        </section>
+    </div>
+
+    <!-- Features 2 -->
+    <div class="wrapper">
+        <section class="container">
+            <header class="major">
+                <h2>Tenemos para ti todos los servicios que necesitas</h2>
+                <p>Queremos darte el servicio que necesitas de la forma más rapida y sencilla.</p>
+            </header>
+            <div class="row features">
+                <section class="col-4 col-12-narrower feature">
+                    <div class="image-wrapper first sub-service-img">
+                        <a href="#" class="image featured"><img src="images/pintor1.jpg" alt="" /></a>
+                    </div>
+                    <p>Pintura de interiores.</p>
+                </section>
+                <section class="col-4 col-12-narrower feature">
+                    <div class="image-wrapper sub-service-img">
+                        <a href="#" class="image featured"><img src="images/masaj1.jpg" alt="" /></a>
+                    </div>
+                    <p>Servicio de masajista a domicilio.</p>
+                </section>
+                <section class="col-4 col-12-narrower feature">
+                    <div class="image-wrapper sub-service-img">
+                        <a href="#" class="image featured"><img src="images/limp1.jpg" alt="" /></a>
+                    </div>
+                    <p>Te mandamos una persona para que limpie lo que necesita tu hogar.</p>
+                </section>
+            </div>
+        </section>
+    </div>
+
+    <!-- Footer -->
+    <div id="footer-wrapper">
+        <div id="footer" class="container">
+            <header class="major">
+                <h2>¿Quieres ponerte en contacto con nosotros?</h2>
+                <p>Mandanos un correo electrónico y te responderemos en la mayor brevedad posible.</p>
+            </header>
+            <div class="row">
+                <section class="col-6 col-12-narrower">
+                    <form method="post" action="#">
+                        <div class="row gtr-50">
+                            <div class="col-6 col-12-mobile">
+                                <input name="name" placeholder="Nombre" type="text" />
+                            </div>
+                            <div class="col-6 col-12-mobile">
+                                <input name="email" placeholder="Email" type="text" />
+                            </div>
+                            <div class="col-12">
+                                <textarea name="message" placeholder="Mensaje"></textarea>
+                            </div>
+                            <div class="col-12">
+                                <ul class="actions">
+                                    <li><input type="submit" value="Enviar mensaje" /></li>
+                                    <li><input type="reset" value="Cancelar" /></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </form>
+                </section>
+                <section class="col-6 col-12-narrower">
+                    <div class="row gtr-0">
+                        <ul class="divided icons col-6 col-12-mobile">
+                            <li class="icon fa-twitter"><a href="#"><span class="extra">twitter.com/</span>untitled</a></li>
+                            <li class="icon fa-facebook"><a href="#"><span class="extra">facebook.com/</span>untitled</a></li>
+                            <li class="icon fa-dribbble"><a href="#"><span class="extra">dribbble.com/</span>untitled</a></li>
+                        </ul>
+                        <ul class="divided icons col-6 col-12-mobile">
+                            <li class="icon fa-instagram"><a href="#"><span class="extra">instagram.com/</span>untitled</a></li>
+                            <li class="icon fa-youtube"><a href="#"><span class="extra">youtube.com/</span>untitled</a></li>
+                            <li class="icon fa-pinterest"><a href="#"><span class="extra">pinterest.com/</span>untitled</a></li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <div id="copyright" class="container">
+            <ul class="menu">
+                <li>&copy; Services. All rights reserved.</li>
+            </ul>
+        </div>
+    </div>
+
+</div>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.dropotron.min.js"></script>
+<script src="assets/js/browser.min.js"></script>
+<script src="assets/js/breakpoints.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
